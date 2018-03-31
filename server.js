314f4a0981dc062a5720536f43560637ac546c6e
makakura <shaharaki@gmail.com>
2018-03-31 23:49:36 +0700

    a

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-// //Install express server
-// const express = require('express');
-// const app = express();
+//Install express server
+const express = require('express');
+const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist'));
-
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-// server.js
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 const path = require('path');
 // ...
 // For all GET requests, send back index.html
@@ -16,3 +11,5 @@ const path = require('path');
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
