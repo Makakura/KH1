7a8664979ffef1f7f114a8e402b437599dd01850
makakura <shaharaki@gmail.com>
2018-04-01 00:01:51 +0700

    123

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
 const path = require('path');
 // ...
 // For all GET requests, send back index.html
@@ -11,5 +15,3 @@ const path = require('path');
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
